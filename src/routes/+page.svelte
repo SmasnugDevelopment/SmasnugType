475541aa97aafec963f234512b9fc945c6b8cd00
Ingo <me@ingowolf.au>
2025-03-05 18:15:42 +1100

    add some code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let inputText = '';
+
+	function keyDown(e: KeyboardEvent) {
+		inputText += e.key;
+		console.log(inputText);
+	}
+</script>
+
+<svelte:head>
+	<title>SmasnugType</title>
+</svelte:head>
+
+<svelte:document onkeypress={keyDown} />
